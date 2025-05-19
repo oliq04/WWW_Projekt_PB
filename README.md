461e10039bf8ab45ca5ce07204fba2a38e3389c7
Oliwier Zyskowski <oliwierzyskowski04@gmail.com>
2025-05-20 01:23:44 +0200

    Usuniecie zbednego kodu

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Jak odpalić json-server?
 
 1. Zainstaluj Node.js ([text](https://nodejs.org/en))
 2. W wierszu polecen: npm install -g json-server
-3. Następnie: json-server --watch baza.json --port 3000
+3. Następnie w folderze z repo: json-server baza.json --watch --port 3000
 4. Serwer stoi na porcie 3000, sprawdź endpoint: http://localhost:3000/ksiazka
--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-﻿const images = [
-    "https://raw.githubusercontent.com/Kazu-yuki/zdjecia/refs/heads/main/images/04338A03865KS_HD.jpg.webp",
-    "https://raw.githubusercontent.com/Kazu-yuki/zdjecia/refs/heads/main/images/harry_potter_i_kamien_filozoficzny_duddle.webp",
-    "https://raw.githubusercontent.com/Kazu-yuki/zdjecia/refs/heads/main/konrad-wallenrod_m9C4Rls.webp",
-    "https://raw.githubusercontent.com/Kazu-yuki/zdjecia/refs/heads/main/w512_u90.webp"
-];
-
-let index = 0;
-function changeImage() {
-    document.getElementById("zdjksiazki").src = images[index];
-    index = (index + 1) % images.length;
-}
-
-changeImage(); // Załaduj pierwsze zdjęcie
-setInterval(changeImage, 5000); // Zmieniaj co 15 sekundy
-
-
-function zegar(){
-        var data = new Date();
-        var godzina = data.getHours();
-        var min = data.getMinutes();
-        var sec = data.getSeconds();
-        var teraz = +godzina+
-        ((min<10)?":0":":")+min+
-        ((sec<10)?":0":":")+sec;
-        document.getElementById("czas").innerHTML = teraz;
-        setTimeout("zegar()", 1000);
-}
-zegar(); // Wywołaj funkcję zegar() przy załadowaniu strony
-
-
+﻿
 
 function darkMode()
 {
