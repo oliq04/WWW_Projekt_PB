b0cf17d540dcb7ada75b21118671cd1823aaa8b0
Oliwier Zyskowski <oliwierzyskowski04@gmail.com>
2025-05-28 00:58:32 +0200

    Proba implementacji ulubionych ksiazek w local storage

--- script.js
+++ script.js
@@ -14,3 +14,39 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+
+
+function nowyObiekt(ksiazka)
+{
+    const miejsce = document.getElementById("usereview");
+
+    const tyt = document.createElement("p");
+    tyt.className = "tytul-ksiazki";
+
+    const divKsiazka = document.createElement("div");
+    divKsiazka.className = "ramka-dla-obiektu";
+
+    const imgObiekt = document.createElement("img");
+    imgObiekt.className = "obrazek-ksiazki";
+
+    const przycisk = document.createElement("button");
+    przycisk.className = "przycisk-usun";
+    przycisk.innerText = "Usuń";
+    przycisk.onclick = function ()
+    {
+      localStorage.removeItem(ksiazka.tytul);
+      miejsce.removeChild(divKsiazka);
+    }
+
+    imgObiekt.src=ksiazka.link;
+    imgObiekt.alt=ksiazka.tytul;
+
+
+    tyt.innerText=ksiazka.tytul;
+
+    divKsiazka.appendChild(tyt);
+    divKsiazka.appendChild(imgObiekt);
+
+    miejsce.appendChild(divKsiazka);
+  
+}
